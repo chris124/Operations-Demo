2a83a4773a166df5f7ac9866585cd172ae4e622a
Ankit Gupta <ag@alphonsolabs.com>
2011-06-06 16:40:58 -0700

    Added notifications

--- OperationsDemo/RootViewController.h
+++ OperationsDemo/RootViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface RootViewController : UITableViewController {
 
+    NSMutableDictionary *websites;
+    NSMutableDictionary *websiteData;
+    NSOperationQueue *operationQueue;
 }
 
 
--- OperationsDemo/RootViewController.m
+++ OperationsDemo/RootViewController.m
@@ -8,11 +8,40 @@
 
 #import "RootViewController.h"
 
+#import "DisplayViewController.h"
+#import "DownloadUrlOperation.h"
+
 @implementation RootViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    websites = [[NSMutableDictionary dictionaryWithCapacity:5] retain];
+    [websites setValue:@"http://google.com" forKey:@"Google"];
+    [websites setValue:@"http://wikipedia.org" forKey:@"Wikipedia"];
+    [websites setValue:@"http://pulse.me" forKey:@"Pulse.me"];
+    [websites setValue:@"http://apple.com" forKey:@"Apple"];
+    [websites setValue:@"http://twitter.com" forKey:@"Twitter"];
+
+    self.title = @"Operations Demo";
+    
+    websiteData = [[NSMutableDictionary dictionaryWithCapacity:5] retain];
+
+    // Create operation queue
+    operationQueue = [NSOperationQueue new];
+    // set maximum operations possible
+    [operationQueue setMaxConcurrentOperationCount:2];
+    
+    // Add operations to download data
+    for (NSString *key in [websites allKeys]) {
+        NSString *urlAsString = [websites valueForKey:key];
+        DownloadUrlOperation *operation = [[DownloadUrlOperation alloc] initWithURL:[NSURL URLWithString:urlAsString]];
+        [operation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:NULL];
+        [operationQueue addOperation:operation]; // operation starts as soon as its added
+        [operation release];
+    }
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -51,7 +80,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [[websites allKeys] count];
 }
 
 // Customize the appearance of table view cells.
@@ -65,59 +94,26 @@
     }
 
     // Configure the cell.
+    NSString *key = [[websites allKeys] objectAtIndex:[indexPath row]];
+    cell.textLabel.text = key;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete)
-    {
-        // Delete the row from the data source.
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }
-    else if (editingStyle == UITableViewCellEditingStyleInsert)
-    {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    /*
-    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-    // ...
-    // Pass the selected object to the new view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
-    [detailViewController release];
-	*/
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    DisplayViewController *vc = [[DisplayViewController alloc] initWithNibName:@"DisplayViewController" bundle:nil];
+    NSString *key = [[websites allKeys] objectAtIndex:[indexPath row]];
+    vc.sourceTitle = key;
+    if ([websiteData objectForKey:key]) {
+        vc.data = [websiteData objectForKey:key];
+    }
+    [self.navigationController pushViewController:vc animated:YES];
+    [vc release];
 }
 
 - (void)didReceiveMemoryWarning
@@ -138,7 +134,49 @@
 
 - (void)dealloc
 {
+    [websites release];
     [super dealloc];
 }
 
+#pragma mark -
+#pragma Actions
+- (void)cancel:(id)sender {
+    [operationQueue cancelAllOperations];
+}
+
+#pragma mark -
+#pragma KVO Observing
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)operation change:(NSDictionary *)change context:(void *)context {
+    if ([operation isKindOfClass:[DownloadUrlOperation class]]) {
+        DownloadUrlOperation *downloadOperation = (DownloadUrlOperation *)operation;
+        NSString *source = nil;
+        for (NSString *key in [websites allKeys]) {
+            if ([[websites valueForKey:key] isEqualToString:[downloadOperation.connectionURL absoluteString]]) {
+                source = key;
+                break;
+            }
+        }
+        if (source) {
+            NSLog(@"Downloaded finished from %@", source);
+            NSData *data = [downloadOperation data];
+            NSError * error = [downloadOperation error];
+            if (error != nil) {
+                // handle error
+                // Notify that we have got an error downloading this data;
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"DataDownloadFailed"
+                                                                    object:self
+                                                                  userInfo:[NSDictionary dictionaryWithObjectsAndKeys:source, @"source", error, @"error", nil]]; 
+            
+            } else {
+                // Notify that we have got this source data;
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"DataDownloadFinished"
+                                                                    object:self
+                                                                  userInfo:[NSDictionary dictionaryWithObjectsAndKeys:source, @"source", data, @"data", nil]]; 
+                // save data
+                [websiteData setValue:data forKey:source];
+            }
+
+        }
+    }
+}
 @end
